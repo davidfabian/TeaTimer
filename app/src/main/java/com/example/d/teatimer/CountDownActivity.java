e0408a8d7a79cc4a328d6092b0e76f3f81ed0792
davidfabian <fabiand.01@gmail.com>
2017-11-16 16:26:47 +0300

    refactored whole app, bug fixes.

--- app/src/main/java/com/example/d/teatimer/CountDownActivity.java
+++ app/src/main/java/com/example/d/teatimer/CountDownActivity.java
@@ -1,11 +1,18 @@
 package com.example.d.teatimer;
 
 import android.content.Context;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.os.Vibrator;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,50 +23,97 @@ import android.widget.Toast;
 
 public class CountDownActivity extends AppCompatActivity {
 
+    final static int GREEN_TEA = 10;
+    final static int BLACK_TEA = 20;
     TextView mTextField;
     Long teaTimer;
     String teatype;
+    Ringtone ringring;
+    Vibrator vibri;
+    long[] pattern = {300, 400};
+    CountDownTimer timer;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.countdown);
-        mTextField = findViewById(R.id.tv_counter);
+        //get context
         final Context context = getApplicationContext();
-//getting the extra to decide tea type.
+        //attach views to variables
+        final LinearLayout mRootView = findViewById(R.id.countdown_main);
+        final TextView mTextField = findViewById(R.id.tv_counter);
+        final Button mStopButton = findViewById(R.id.stop_button);
+
+        //initiate vibri, the vibrator
+        vibri = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+
+        //get reference to alarm sound
+        Uri notification = RingtoneManager.getActualDefaultRingtoneUri(context, RingtoneManager.TYPE_ALARM);
+        ringring = RingtoneManager.getRingtone(context, notification);
+
+        //getting the intent extra to decide tea type.
+        //setting tea variables (color, brewing time, tea name
         final int teaExtra = getIntent().getExtras().getInt("teaExtra");
 
-        if (teaExtra == 10) {
-            teaTimer = 180000L;
-            mTextField.setBackgroundResource(R.color.greentea);
-            teatype = "Green Tea";
+        if (teaExtra == GREEN_TEA) {
+            teaTimer = 3000L;
+            mRootView.setBackgroundResource(R.color.greentea);
+            teatype = getResources().getString(R.string.green_tea);
             setTitle(teatype);
 
-        } else {
-            teaTimer = 300000L;
-            mTextField.setBackgroundResource(R.color.blacktea);
-            teatype = "Black Tea";
+        } else if (teaExtra == BLACK_TEA) {
+            teaTimer = 5000L;
+            mRootView.setBackgroundResource(R.color.blacktea);
+            teatype = getResources().getString(R.string.black_tea);
             setTitle(teatype);
         }
+        //something went wrong. invalid tea type
+        else {
+            finish();
+        }
 
+        //a new instance of countdowntimer, ticks every second
+        timer = new CountDownTimer(teaTimer, 1000) {
 
-        final Vibrator vibri = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
-
-        new CountDownTimer(teaTimer, 1000) {
-
+            //refreshing remaining time every second,
             public void onTick(long millisUntilFinished) {
-                mTextField.setText("The " + teatype + " will be ready in " + millisUntilFinished / 1000 + " seconds!");
+                mTextField.setText(teatype + " " + getResources().getString(R.string.will_be_ready) + " " + millisUntilFinished / 1000 + " " + getResources().getString(R.string.seconds));
             }
 
+            //when timer runs out, change button text start alarm and display a toast
             public void onFinish() {
-                long[] pattern = {300, 400, 300, 400, 300, 400, 300, 400, 300, 400};
-                vibri.vibrate(pattern, -1);
-
-                Toast.makeText(context, teatype + " ready!", Toast.LENGTH_LONG).show();
-                mTextField.setText("done!");
+                //set button text to teatype ready
+                mStopButton.setText(teatype + " " + getResources().getString(R.string.ready));
+                //start alarm and vibration
+                startAlarm();
+                //display toast
+                Toast toast = Toast.makeText(context, teatype + " " + getResources().getString(R.string.ready), Toast.LENGTH_LONG);
+                toast.setGravity(Gravity.CENTER_VERTICAL | Gravity.CENTER_HORIZONTAL, 0, 50);
+                toast.show();
+                //replace countdown timer with Ready!
+                mTextField.setText(R.string.ready);
             }
+            //start the timer
         }.start();
-
-
+        //after timer started, set button text to cancel
+        mStopButton.setText(R.string.cancel_timer);
     }
+
+    //executed when alarm starts: starts vibrating and playing alarm sound repeatedly
+    public void startAlarm() {
+
+        vibri.vibrate(pattern, 0);
+        ringring.play();
+        timer.cancel();
+    }
+
+    //stop the alarms or cancel countdown, exit activity
+    public void stopAlarm(View view) {
+        timer.cancel();
+        ringring.stop();
+        vibri.cancel();
+        finish();
+    }
+
+
 }
--- app/src/main/java/com/example/d/teatimer/MainActivity.java
+++ app/src/main/java/com/example/d/teatimer/MainActivity.java
@@ -1,33 +1,23 @@
 package com.example.d.teatimer;
 
-import android.content.Context;
 import android.content.Intent;
-import android.os.CountDownTimer;
-import android.os.VibrationEffect;
-import android.os.Vibrator;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Chronometer;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-
-import static android.os.VibrationEffect.createOneShot;
 
 public class MainActivity extends AppCompatActivity {
 
 Button greentea;
 Button blacktea;
 
-private TextView mTextField;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        greentea =  (Button) findViewById(R.id.green_tea_button);
-        blacktea =  (Button) findViewById(R.id.black_tea_button);
+        greentea = findViewById(R.id.green_tea_button);
+        blacktea = findViewById(R.id.black_tea_button);
 
         // Set a click listener for greentea button
         greentea.setOnClickListener(new View.OnClickListener() {
@@ -37,6 +27,7 @@ private TextView mTextField;
                 Intent countdownIntent= new Intent(MainActivity.this, CountDownActivity.class);
                 countdownIntent.putExtra("teaExtra", 10);
                 startActivity(countdownIntent);
+                finish();
             }
         });
         blacktea.setOnClickListener(new View.OnClickListener() {
@@ -46,6 +37,7 @@ private TextView mTextField;
                 Intent countdownIntent= new Intent(MainActivity.this, CountDownActivity.class);
                 countdownIntent.putExtra("teaExtra", 20);
                 startActivity(countdownIntent);
+                finish();
             }
         });
     }
